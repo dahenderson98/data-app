a5615f3efaa0370ea54539902767fdab74a1f2fe
Dallin Henderson <70783126+dahenderson98@users.noreply.github.com>
2024-11-05 15:59:20 -0700

    Ensure StoreData is called

--- src/DataApp.Tests/DataManagerTests.cs
+++ src/DataApp.Tests/DataManagerTests.cs
@@ -41,29 +41,38 @@ public class DataManagerTests
     public void TestValidDataId(int dataId)
     {
         // Arrange
-        var dataManager = GetDataManager(stringInFile: "valid data");
+        Mock<IDataStorage> storageVerifiable = null;
+        var dataManager = GetDataManager(stringInFile: "valid data", ref storageVerifiable);
 
         // Act
         var result = dataManager.ConsolidateDataFromSources(dataId);
 
         // Assert
         Assert.Equal(0, result);
+        storageVerifiable.Verify();
     }
 
     private DataManager GetDataManager(string stringInFile)
+    {
+        Mock<IDataStorage> storageVerifiable = null;
+        return GetDataManager(stringInFile, ref storageVerifiable);
+    }
+
+    private DataManager GetDataManager(string stringInFile, ref Mock<IDataStorage> storageVerifiable)
     {
         // Mock DataFetcher        
         var mockedFetcher = new Mock<IDataFetcher>();
         mockedFetcher.Setup(x => x.FetchData(It.IsAny<int>())).Returns(stringInFile);
 
-        // Mock DataGetter
-        var mockedStorage = new Mock<IDataStorage>();
-        mockedStorage.Setup(x => x.StoreData(It.IsAny<int>(), It.IsAny<string>())).Verifiable();
+        // Mock DataStorage
+        storageVerifiable = new Mock<IDataStorage>();
+        storageVerifiable.Setup(x => x.StoreData(It.IsAny<int>(), It.IsAny<string>())).Verifiable();
 
+        // Inject dependencies
         return new DataManager()
         {
             DataFetcher = mockedFetcher.Object,
-            DataStorage = mockedStorage.Object
+            DataStorage = storageVerifiable.Object
         };
     }
 }
